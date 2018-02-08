f05e98f9d744fbbd921dc6cb7c2640267c031c86
Adam Beziou <adam.beziou@gmail.com>
2018-02-08 12:13:51 -0700

    Added comments

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -92,18 +92,18 @@ int CheckWin(vector< vector<int> > gameBoard) { // Checks for win conditions
     return win; // Returns win state
 }
 
+/* Below is all of the CPU process garbage */
+int AdjacentCheck(int i, int j, int tempDecisionValue, vector<vector<int> > & gameBoard) { // Checks for adjacent squares of the same type, modifying the CPU's decision value
 
-int AdjacentCheck(int i, int j, int tempDecisionValue, vector<vector<int> > & gameBoard) {
+    /* Declares/initializes variables for the for-loops and check values */
     int x = 0;
     int y = 0;
     int checkValue = 0;
 
-    cout << i << " " << j << endl;
-    cout << "Check" << endl;
-
+    /* For-loops to check adjacent squares */
     for (x = -1; x < 2; x++) {
         for (y = -1; y < 2; y++) {
-            if (y != 0 && x != 0) {
+            if (y != 0 && x != 0) { // Prevents a square from checking itself
                 try {
                     checkValue = gameBoard.at(i + x).at(j + y); // Checks every adjacent square
 
@@ -117,14 +117,14 @@ int AdjacentCheck(int i, int j, int tempDecisionValue, vector<vector<int> > & ga
                         tempDecisionValue += 2;
                     }
                 }
-                catch (const out_of_range& e) {
+                catch (const out_of_range& e) { // Catches out of range errors
                     continue;
                 }
             }
         }
     }
 
-    return tempDecisionValue;
+    return tempDecisionValue; // Returns the decision value
 }
 
 bool CPUMove(vector<vector<int> > & gameBoard){
@@ -180,7 +180,6 @@ bool CPUMove(vector<vector<int> > & gameBoard){
                                 }
                                 else if (checkValue == 1) {
                                     tempDecisionValue += 2;
-                                    cout << i + x << " " << j + y;
                                     tempDecisionValue += AdjacentCheck(i + x, j + y, tempDecisionValue, gameBoard);
                                 }
                                 else if (checkValue == 2) {
@@ -188,7 +187,7 @@ bool CPUMove(vector<vector<int> > & gameBoard){
                                     tempDecisionValue += AdjacentCheck(i + x, j + y, tempDecisionValue, gameBoard);
                                 }
                             }
-                            catch (const out_of_range& e) {
+                            catch (const out_of_range& e) { // Catches out of range errors
                                     continue;
                             }
                         }
@@ -196,7 +195,7 @@ bool CPUMove(vector<vector<int> > & gameBoard){
                 }
             }
 
-            if (tempDecisionValue > decisionValue) {
+            if (tempDecisionValue > decisionValue) { // If the current square's decision value is larger than the previous largest squares, replaces the previous position with the current
                 decisionValue = tempDecisionValue;
                 maxI = i;
                 maxJ = j;
@@ -204,11 +203,14 @@ bool CPUMove(vector<vector<int> > & gameBoard){
         }
     }
 
-    gameBoard[maxI][maxJ] = 2;
+    gameBoard[maxI][maxJ] = 2; // Changes the square with the largest decision value
     return tieCheck;
 }
 
+/* Below is the player decision garbage */
 bool UpdateScreen(vector<vector<int> > & gameBoard, int numPlayer) {
+
+    /* Declares/initializes varables for for-loops, choices, etc. */
     int counter = 1;
     int gameChoice = 0;
     int i = 0;
@@ -219,43 +221,42 @@ bool UpdateScreen(vector<vector<int> > & gameBoard, int numPlayer) {
 
     bool tieCheck = true;
     
-    cout << string(50, '\n');
-    cout << "Player " << numPlayer << endl
+    cout << string(50, '\n'); // Creates a large space to separate different actions
+    cout << "Player " << numPlayer << endl // Displays the urrent player's number
         << "--------" << endl << endl;
 
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < 3; i++) { // Checks if each square is empty/available
         for (j = 0; j < 3; j++) {
-            if (gameBoard[i][j] == 0) {
-                cout << counter;
+            if (gameBoard[i][j] == 0) { // IF a square is available, increments the counter (used for diaplayying choices), changes the tie checker to false, and appends the i/j value vectors with its position
                 counter++;
                 tieCheck = false;
 
                 iVals.push_back(i);
                 jVals.push_back(j);
             }
-            else if (gameBoard[i][j] == 1) {
+            else if (gameBoard[i][j] == 1) { // IF the square is owned/claimed by player 1, displays an X
                 cout << "X";
             }
-            else {
+            else { // If the square is owned/claimed by player 2/CPU, displays a O
                 cout << "O";
             }
 
-            if (j != 2) {
+            if (j != 2) { // Separates adjacent symbols
                 cout << "|";
             }
         }
-        if (i != 2) {
+        if (i != 2) { // Separates adjacent rows
             cout << endl << "-|-|-" << endl;
         }
     }
         
-    if (tieCheck == true) {
+    if (tieCheck == true) { // Tells the game function if there is a tie before prompting for a chooice
         return tieCheck;
     }
 
     else {
 
-        cout << endl << endl << "Valid Spaces: ";
+        cout << endl << endl << "Valid Spaces: "; // Lists the valid spaces
             
         for (i = 1; i < counter; i++) {
             if (i < counter - 1) {
@@ -266,14 +267,14 @@ bool UpdateScreen(vector<vector<int> > & gameBoard, int numPlayer) {
             }
         }
             
-        cout << endl << "Selection: ";
+        cout << endl << "Selection: "; // Prompts the user for a selection
 
         do {
             cin >> gameChoice;
-        } while (!(gameChoice >= 1 && gameChoice <= counter - 1));
+        } while (!(gameChoice >= 1 && gameChoice <= counter - 1)); // Checks for a valid input
 
 
-        if (numPlayer == 1) {
+        if (numPlayer == 1) { // IF player == 1, replaces choice with an X, else replaces with an O
             gameBoard[iVals.at(gameChoice - 1)][jVals.at(gameChoice - 1)] = 1;
         }
         else {
@@ -285,27 +286,31 @@ bool UpdateScreen(vector<vector<int> > & gameBoard, int numPlayer) {
 }
 
 
+/* Below is the function for a PVP game */
 void PvPGame() {
+
+    /* Declares variables for the game board, tie checking, win state, etc. */
     vector< vector<int> > gameBoard(3, vector<int>(3));
+
     int tieCheck;
     int numPlayer = 1;
-    bool human = true;
+
     bool win = false;
 
-    while (win == false) {
-        tieCheck = UpdateScreen(gameBoard, numPlayer);
+    while (win == false) { // While nobody has won...
+        tieCheck = UpdateScreen(gameBoard, numPlayer); // Prompts the current playyer for a choice, checks if there is a tie
 
-        if (tieCheck == true) {
+        if (tieCheck == true) { // Breaks if there is a tie
             cout << endl << endl << "It's a tie!" << endl << endl;
             break;
         }
 
-        win = CheckWin(gameBoard);
+        win = CheckWin(gameBoard); // Checks if anyone won if not
 
-        if (win == false) {
+        if (win == false) { // If nobody won, changes the player number and repeats
             numPlayer = (numPlayer % 2) + 1;
         }
-        else {
+        else { // IF somebody won, break and display who won
             cout << endl << endl << "Player " << numPlayer << " has won!" << endl << endl;
         }
     }
@@ -314,27 +319,33 @@ void PvPGame() {
 }
 
 
+/* Below is the function for PVCPU games */
 void PvCPUGame() {
+
+    /* Declares variabes for the game board, tie checking, win state, etc. */
     vector< vector<int> > gameBoard(3, vector<int>(3));
+
     int tieCheck;
     int numPlayer = 1;
+
     bool win = false;
 
-    do {
-        tieCheck = UpdateScreen(gameBoard, numPlayer);
+    do { // While true...
+        tieCheck = UpdateScreen(gameBoard, numPlayer); // Prompts the user for a decision and checks if there is a tie
 
-        win = CheckWin(gameBoard);
+        win = CheckWin(gameBoard); // Checks if anyone won
 
-        if (win == true) {
+        if (win == true) { // IF the player has won, break and tell them
             cout << endl << endl << "You've won!" << endl << endl;
             return;
         }
 
-        else if (tieCheck == true) {
+        else if (tieCheck == true) { // IF there is a tie, break and tell the player
             cout << endl << endl << "It's a tie!" << endl << endl;
             return;
         }
 
+        // Else, repeat the process for the CPU
         tieCheck = CPUMove(gameBoard);
 
         win = CheckWin(gameBoard);
@@ -351,14 +362,18 @@ void PvCPUGame() {
     } while (true);
 }
 
+
+/* Below is the main function */
 int main() {
+
+    /* Declares the variables for the game choice and repeating choice */
     char gameChoice = '0';
     char continueChoice = 'y';
 
-    do {
-        cout << string(50, '\n');
+    do { // While the player wants to repeat/when the game starts...
+        cout << string(50, '\n'); // Create some spacing
 
-        cout << "Tic-Tac-Toe" << endl
+        cout << "Tic-Tac-Toe" << endl // List choices for games
              << "-----------" << endl
              << "Select a game:" << endl
              << "1) Player vs Player" << endl
@@ -367,11 +382,11 @@ int main() {
              << "Selection: ";
 
         do {
-            cin.get(gameChoice);
+            cin.get(gameChoice); // Get game choice and check for invalid inputs
         } while (gameChoice != '1' && gameChoice != '2' && gameChoice != '3');
 
     
-        switch(gameChoice) {
+        switch(gameChoice) { // Checks what game the player wants to play
             case '1':
                 PvPGame();
                 break;
@@ -386,7 +401,7 @@ int main() {
                 break;
         }
 
-        cout << "Do you want to play again? (y/n)" << endl;
+        cout << "Do you want to play again? (y/n)" << endl; // Once a game is over, asks the player is they want to play again
         cout << "Selection: ";
         
         do {
